4f81186d30afcff81406f3272b5ac030b8e85a91
Gaspar <e972136@gmail.com>
2025-09-04 16:35:36 -0600

    ini

---
+++ src/main/java/com/gaspar/main/JewEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.gaspar.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewEncryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaspar/main/JewEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspar.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
